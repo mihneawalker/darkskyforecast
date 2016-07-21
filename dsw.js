2e7086fb473e7daa76c42ea87d675daac2c71c6c
mihneawalker <mihnea.beldescu@gmail.com>
2016-07-21 18:20:51 +0300

    done!

--- dsw.js
+++ dsw.js
@@ -1,13 +1,18 @@
+//simple NodeJS weather getter for darksky forecast.io
 
 var request = require('request');
 
-qString = 'https://api.forecast.io/forecast/a1e8b238a7079186703dc8429746331a/44.4322500,26.1062600?units=si';
+var apiEndpoint = 'https://api.forecast.io/forecast/';
+var apiKey      = 'a1e8b238a7079186703dc8429746331a';
+var latitude    = '44.4322500';
+var longitude   = '26.1062600'
+var options     = 'units=si';
+
+qString = apiEndpoint + apiKey + '/' + latitude +','+ longitude +'?'+ options;
+console.log(qString);
 
 request(qString, function(error,response, body) {
-	if (!error && response.statusCode == 200) {
-		console.log(body);
-	}
+    if (!error && response.statusCode == 200) {
+        console.log(body);
+    }
 });
-
-
-
